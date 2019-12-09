a4d1aa0874180d397d811684770a61dd3ebd5a08
Kacper Bak <github@kacperbak.de>
2019-12-09 22:20:16 +0100

    routerprefix removed, default page disabled, tried to implement root mapping

--- RootMappedProject/MyRootMappedFunction.cs
+++ RootMappedProject/MyRootMappedFunction.cs
@@ -1,33 +1,30 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace RootMappedProject
 {
     public static class MyRootMappedFunction
     {
-        [FunctionName("MyRootMappedFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("Simple")]
+        public static async Task<IActionResult> Simple(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Processing simple endpoint ...");
+            return (ActionResult)new OkObjectResult("true");
+        }
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        [FunctionName("Root")]
+        public static async Task<IActionResult> Root(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Processing root endpoint, this is a bad request");
+            return (ActionResult) new BadRequestObjectResult("false");
         }
     }
 }
